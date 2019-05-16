8380e986d614bf0974527c3e7db46dc0e28f62ef
sheebypanda <sheebypanda@gmail.com>
2019-05-16 17:57:10 +0200

    clearing controlers

--- app/controllers/priorities_controller.rb
+++ app/controllers/priorities_controller.rb
@@ -1,34 +1,26 @@
 class PrioritiesController < ApplicationController
   before_action :set_priority, only: [:show, :edit, :update, :destroy]
 
-  # GET /priorities
-  # GET /priorities.json
   def index
     @priorities = Priority.all
   end
 
-  # GET /priorities/1
-  # GET /priorities/1.json
   def show
   end
 
-  # GET /priorities/new
   def new
     @priority = Priority.new
   end
 
-  # GET /priorities/1/edit
   def edit
   end
 
-  # POST /priorities
-  # POST /priorities.json
   def create
     @priority = Priority.new(priority_params)
 
     respond_to do |format|
       if @priority.save
-        format.html { redirect_to @priority, notice: 'Priority was successfully created.' }
+        format.html { redirect_to priorities_path, notice: 'Priority was successfully created.' }
         format.json { render :show, status: :created, location: @priority }
       else
         format.html { render :new }
@@ -37,8 +29,6 @@ class PrioritiesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /priorities/1
-  # PATCH/PUT /priorities/1.json
   def update
     respond_to do |format|
       if @priority.update(priority_params)
@@ -51,8 +41,6 @@ class PrioritiesController < ApplicationController
     end
   end
 
-  # DELETE /priorities/1
-  # DELETE /priorities/1.json
   def destroy
     @priority.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class PrioritiesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_priority
       @priority = Priority.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def priority_params
       params.require(:priority).permit(:name, :value)
     end
--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,34 +1,26 @@
 class SubjectsController < ApplicationController
   before_action :set_subject, only: [:show, :edit, :update, :destroy]
 
-  # GET /subjects
-  # GET /subjects.json
   def index
     @subjects = Subject.all
   end
 
-  # GET /subjects/1
-  # GET /subjects/1.json
   def show
   end
 
-  # GET /subjects/new
   def new
     @subject = Subject.new
   end
 
-  # GET /subjects/1/edit
   def edit
   end
 
-  # POST /subjects
-  # POST /subjects.json
   def create
     @subject = Subject.new(subject_params)
 
     respond_to do |format|
       if @subject.save
-        format.html { redirect_to @subject, notice: 'Subject was successfully created.' }
+        format.html { redirect_to subjects_path, notice: 'Subject was successfully created.' }
         format.json { render :show, status: :created, location: @subject }
       else
         format.html { render :new }
@@ -37,8 +29,6 @@ class SubjectsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /subjects/1
-  # PATCH/PUT /subjects/1.json
   def update
     respond_to do |format|
       if @subject.update(subject_params)
@@ -51,8 +41,6 @@ class SubjectsController < ApplicationController
     end
   end
 
-  # DELETE /subjects/1
-  # DELETE /subjects/1.json
   def destroy
     @subject.destroy
     respond_to do |format|
@@ -62,13 +50,11 @@ class SubjectsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_subject
       @subject = Subject.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def subject_params
-      params.require(:subject).permit(:name, :parent_it)
+      params.require(:subject).permit(:name, :parent_id)
     end
 end
